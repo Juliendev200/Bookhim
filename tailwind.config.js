535f76dc23fb07c3ea09444b3824fc001fcbea12
Julien <julien.lefebvre@epitech.eu>
2024-09-29 09:03:43 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+
+    extend: {
+      colors: {
+        'main-color': '#0065FC',
+        'main-bg-color': '#F2F2F2',
+        'filter-bg-color': '#DEEBFF',
+      },
+    },
+  },
+  plugins: [],
+}
+
